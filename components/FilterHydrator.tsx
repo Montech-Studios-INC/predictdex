6486e1ee53d66e0f0237b7e38b79c48c992e0a87
Grey Jabesi <jabesigrey@gmail.com>
2025-11-24 20:04:07 +0400

    chore: temporarily disable route filter hydrator

--- components/FilterHydrator.tsx
+++ components/FilterHydrator.tsx
@@ -1,31 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
-import { usePredictionStore } from "@/lib/stores/usePredictionStore";
-
 type Props = {
   country?: string | null;
   category?: string | null;
 };
 
-export default function FilterHydrator({ country = null, category = null }: Props) {
-  useEffect(() => {
-    const { setCountryFilter } = usePredictionStore.getState();
-    setCountryFilter(country);
-  }, [country]);
-
-  useEffect(() => {
-    const { setCategoryFilter } = usePredictionStore.getState();
-    setCategoryFilter(category);
-  }, [category]);
-
-  useEffect(() => {
-    const { setCountryFilter, setCategoryFilter } = usePredictionStore.getState();
-    return () => {
-      setCountryFilter(null);
-      setCategoryFilter(null);
-    };
-  }, []);
-
+// Temporarily disabled while we refine the filter synchronisation.
+export default function FilterHydrator(_props: Props) {
   return null;
 }
